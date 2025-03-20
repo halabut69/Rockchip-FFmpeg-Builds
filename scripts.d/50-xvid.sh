25378f985ecc5c38f5aea6189d964d0c751bdc30
halabut69 <155487809+halabut69@users.noreply.github.com>
2025-03-20 12:42:08 +0100

    disable xvid

--- scripts.d/50-xvid.sh
+++ scripts.d/50-xvid.sh
@@ -1,54 +1,57 @@
 #!/bin/bash
 
-SCRIPT_REPO="https://svn.xvid.org/trunk/xvidcore"
-SCRIPT_REV="2202"
+#SCRIPT_REPO="https://svn.xvid.org/trunk/xvidcore"
+#SCRIPT_REV="2202"
 
 ffbuild_enabled() {
-    [[ $VARIANT == lgpl* ]] && return -1
+#    [[ $VARIANT == lgpl* ]] && return -1
     return 0
 }
 
 ffbuild_dockerdl() {
-    echo "retry-tool sh -c \"rm -rf xvid && svn --non-interactive checkout --username 'anonymous' --password '' '${SCRIPT_REPO}@${SCRIPT_REV}' xvid\" && cd xvid"
+#    echo "retry-tool sh -c \"rm -rf xvid && svn --non-interactive checkout --username 'anonymous' --password '' '${SCRIPT_REPO}@${SCRIPT_REV}' xvid\" && cd xvid"
+    return 0
 }
 
 ffbuild_dockerbuild() {
-    cd build/generic
+    return 0
+   # cd build/generic
 
     # The original code fails on a two-digit major...
-    sed -i\
-        -e 's/GCC_MAJOR=.*/GCC_MAJOR=10/' \
-        -e 's/GCC_MINOR=.*/GCC_MINOR=0/' \
-        configure.in
+    #sed -i\
+     #   -e 's/GCC_MAJOR=.*/GCC_MAJOR=10/' \
+      #  -e 's/GCC_MINOR=.*/GCC_MINOR=0/' \
+       # configure.in
 
-    ./bootstrap.sh
+#    ./bootstrap.sh
 
-    local myconf=(
-        --prefix="$FFBUILD_PREFIX"
-    )
+ #   local myconf=(
+  #      --prefix="$FFBUILD_PREFIX"
+   # )
 
-    if [[ $TARGET == win* || $TARGET == linux* ]]; then
-        myconf+=(
-            --host="$FFBUILD_TOOLCHAIN"
-        )
-    else
-        echo "Unknown target"
-        return -1
-    fi
+    #if [[ $TARGET == win* || $TARGET == linux* ]]; then
+     #   myconf+=(
+      #      --host="$FFBUILD_TOOLCHAIN"
+       # )
+    #else
+     #   echo "Unknown target"
+      #  return -1
+    #fi
 
-    ./configure "${myconf[@]}"
-    make -j$(nproc)
-    make install
+    #./configure "${myconf[@]}"
+    #make -j$(nproc)
+    #make install
 
-    if [[ $TARGET == win* ]]; then
-        rm "$FFBUILD_PREFIX"/{bin/libxvidcore.dll,lib/libxvidcore.dll.a}
-    elif [[ $TARGET == linux* ]]; then
-        rm "$FFBUILD_PREFIX"/lib/libxvidcore.so*
-    fi
+    #if [[ $TARGET == win* ]]; then
+     #   rm "$FFBUILD_PREFIX"/{bin/libxvidcore.dll,lib/libxvidcore.dll.a}
+    #elif [[ $TARGET == linux* ]]; then
+     #   rm "$FFBUILD_PREFIX"/lib/libxvidcore.so*
+    #fi
 }
 
 ffbuild_configure() {
-    echo --enable-libxvid
+    #echo --enable-libxvid
+    echo --disable-libxvid
 }
 
 ffbuild_unconfigure() {
